f35a1426e01ed931b629267002f7def6974f1813
SumeetKamble1503 <51257281+SumeetKamble1503@users.noreply.github.com>
2025-03-20 17:04:22 +0530

    feat : fixed local stuff

--- src/controllers/healthcheckController.js
+++ src/controllers/healthcheckController.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// Controller for setting a user's rank
-exports.HealthCheck = async (req, res) => {
-  try {
-    const result = {
-      msg: "Heathcheck Success",
-      status: true,
-    };
-    return res.status(200).json(result);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-=======
 // Controller for setting a user's rank
 exports.HealthCheck = async (req, res) => {
   try {
@@ -25,4 +11,3 @@ exports.HealthCheck = async (req, res) => {
   }
 };
   
->>>>>>> e397940d8bd2580be6229d91d4827b7e41de09be
